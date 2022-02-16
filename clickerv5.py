f359be67172ad2f3d52bde76e26dc0ed6cd96737
Timo de Gruiter <timo@EDGITNEW.local>
2022-02-16 09:24:08 +0100

    opdracht toegevoegd

---
+++ clickerv5.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+
+gui = tk.Tk()
+gui.title('Clicker v1')
+gui.geometry('250x200')
+gui.config(bg="grey")
+clicks = 0
+check = ''
+
+def colors():
+    if clicks < 0:
+        gui.config(bg='red')
+    elif clicks > 0:
+        gui.config(bg='green')
+    else:
+        gui.config(bg='grey')
+
+def up(e):
+    global clicks,check
+    clicks += 1
+    counter.config(text=clicks)
+    counter.pack()
+    check='up'
+    gui.after(10,colors)
+
+def down(e):
+    global clicks,check
+    clicks -= 1
+    counter.config(text=clicks)
+    counter.pack()
+    check='down'
+    gui.after(10,colors)
+
+def on_enter(e):
+   gui.config(bg='yellow')
+def on_leave(e):
+   gui.config(bg='grey')
+    
+def doubleclick(e):
+    global check, clicks
+    if check == 'up':
+        clicks *= 3
+    elif check == 'down':
+        clicks /=3
+    counter.config(text= clicks)
+
+button_up = tk.Button(gui,text='up', font=('arial',20,'bold'),command=up)
+button_up.pack()
+
+counter = tk.Label(
+    gui,
+    text=clicks
+)
+counter.pack()
+
+button_down = tk.Button(gui,text='down', font=('arial',20,'bold'),command=down)
+button_down.pack()
+
+button_up.bind("<Enter>", on_enter)
+button_up.bind("<Leave>", on_leave)
+button_down.bind("<Enter>", on_enter)
+button_down.bind("<Leave>", on_leave)
+counter.bind('<Double-Button>', doubleclick)
+gui.bind('<Up>',up)
+button_up.bind('<Button-1>',up)
+gui.bind('<Down>',down)
+button_down.bind('<Button-1>',down)
+gui.bind('<space>', doubleclick)
+
+gui.mainloop()
